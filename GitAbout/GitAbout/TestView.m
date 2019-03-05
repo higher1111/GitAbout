ada03db2ec7c6564887baf1b85eb001e5586a2a0
jiangyihan jiang <531182927@qq.com>
2019-03-05 19:48:23 +0800

    测试冲突 by JYH 第二次提交

--- GitAbout/GitAbout/TestView.m
+++ GitAbout/GitAbout/TestView.m
@@ -10,12 +10,12 @@
 
 @implementation TestView
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        NSLog(@"这是我的第二次提交");
+    }
+    return self;
 }
-*/
-
 @end
