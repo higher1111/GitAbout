ce22b2446f2cdb96adc1bbda886e5ebdb9c573e4
jiangyihan jiang <531182927@qq.com>
2019-03-05 19:23:19 +0800

    测试覆盖 by JYH

---
+++ GitAbout/GitAbout/TestView.h
@@ -0,0 +1,17 @@
+//
+//  TestView.h
+//  GitAbout
+//
+//  Created by jiangyihan on 2019/3/5.
+//  Copyright © 2019年 xiaodian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TestView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ GitAbout/GitAbout/TestView.m
@@ -0,0 +1,21 @@
+//
+//  TestView.m
+//  GitAbout
+//
+//  Created by jiangyihan on 2019/3/5.
+//  Copyright © 2019年 xiaodian. All rights reserved.
+//
+
+#import "TestView.h"
+
+@implementation TestView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
