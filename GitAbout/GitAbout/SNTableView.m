d251ce703b1b0409ba472dd5498d2c6418ef0526
程欢 <18112734@cnsuning.com>
2019-03-08 14:28:38 +0800

    新增自定义视图

---
+++ GitAbout/GitAbout/SNTableView.m
@@ -0,0 +1,21 @@
+//
+//  SNTableView.m
+//  GitAbout
+//
+//  Created by 程欢 on 2019/3/8.
+//  Copyright © 2019 xiaodian. All rights reserved.
+//
+
+#import "SNTableView.h"
+
+@implementation SNTableView
+
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        
+    }
+    return self;
+}
+
+@end
